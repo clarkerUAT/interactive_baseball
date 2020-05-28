40f32839279808f6520849e94183caa65b9cca52
Ray <64998877+clarkerUAT@users.noreply.github.com>
2020-05-27 22:23:51 -0500

    Add project files.

---
+++ interactive_baseball/Source.cpp
@@ -0,0 +1,38 @@
+/*This program is an interactive baseball fiction gaem
+The user will be prompted to make a selection and upon making a selection will 
+be given a result based on the game and a random selection from the choice that the player makes. 
+The game will be played from the offensive side only. The goal will be to get three runs before the oposing team
+gets three outs.
+*/
+
+//todo: create local variables
+//name (coach name)
+//runner pos
+//strikeCount
+//ballCount
+//# of outs
+//# of runs
+
+//todo:create a random number value will be based on choice #
+
+//todo: create funcitons to recieve and give values
+//getname
+//setname
+//getRunnerpos
+//setRunnerpos
+//runnerChoice
+//batterChoice
+//setRuns
+//getRuns
+//setOuts
+//getOuts
+//setStrikes
+//getStrikes
+//setBalls
+//getBalls
+
+//todo: create multiple switch statements
+// steal and results
+// stay and results
+// bunt and results
+//swing away and results
